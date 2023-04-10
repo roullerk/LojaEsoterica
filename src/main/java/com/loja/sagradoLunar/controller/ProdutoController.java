7bba40bb766966d51deb9eaaff673cf464d3b990
VictorFRS <victorfrds@gmail.com>
2023-04-10 19:18:52 -0300

    Controllers e repositories de categoria e produto.

---
+++ src/main/java/com/loja/sagradoLunar/controller/ProdutoController.java
@@ -0,0 +1,78 @@
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins)
+public class ProdutoController {
+    
+    @Autowired
+    private ProdutoRepository repository;
+
+    //Faz busca de todos os produtos
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAllProduto(){
+            return ResponseEntity.ok(repository.findAll());
+    }
+
+     //Faz busca de produtos por id
+    @GetMapping("/produto/{id}")
+    public ResponseEntity<Produto> findByIdProduto(@PathVariable long id){
+            return repository.findById(id)
+            .map(resp -> ResponseEntity.ok(resp))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    //Faz busca de produtos por nome
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<Produto>> findByNomeProduto(@PathVariable String nome){
+        return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+    }
+
+    //Busca valores menores ou iguais ao cadastrados na tabela
+    @GetMapping("/preco-menor/{preco}")
+    public ResponseEntity<List<Produto>> findByProdutoPrecoMenor(@PathVariable int preco){
+        return ResponseEntity.ok(repository.findAllByPrecoLessThanEquals(preco));
+    }
+
+
+    //Busca valores maiores ou iguais ao cadastrados na tabela
+    @GetMapping("/preco-maior/{preco}")
+    public ResponseEntity<List<Produto>> findByProdutoPrecoMaior(@PathVariable int preco){
+        return ResponseEntity.ok(repository.findAllByPrecoGreaterThanEquals(preco));
+    }
+
+    //Busca os produtos ativos
+    @GetMapping("/ativo/{ativo}")
+    public ResponseEntity<List<Produto>> findAllByAtivo(@PathVariable boolean ativo){
+        return ResponseEntity.ok(repository.findAllByAtivo(ativo));
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> putProduto(@RequestBody Produto produto){
+        ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> putProduto(@RequestBody Produto produto){
+        return  ResponseEntity.ok(repository.save(produto));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduto(@PathVariable long id){
+        repository.deleteById(id);
+    }
+
+}
---
+++ src/main/java/com/loja/sagradoLunar/repository/ProdutoRepository.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.loja.sagradoLunar.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+    public List<Produto>findAllByNomeContainingIgnoreCase(String nome);
+    
+    public List<Produto>findAllByPrecoLessThanEquals(double preco);
+
+    public List<Produto>findAllByPrecoGreaterThanEquals(double preco);
+
+    @Query(value = "SELECT * FROM tb_produto WHERE ativo = :ativo", nativeQuery = true)
+    public List<Produto> findAllByAtivo(@Param("ativo") boolean ativo);
+}
